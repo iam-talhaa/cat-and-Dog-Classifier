c601887753217d0a39b383b688f1c86e0dc5e5c7
Talha Khan <tahakhan4141@gmail.com>
2025-03-13 10:54:51 -0700

    Updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cat_and_dog_classifier/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const cat_and_dog_Classifier());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
